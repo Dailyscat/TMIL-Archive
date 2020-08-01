87dcf404c73a62f88d3158cbbe0e3a444daa2a0d
dailyscat <pay_nack@naver.com>
2020-08-01 17:43:06 +0900

    적립포인트 계산 코드 추출

--- Refactoring/statement.js
+++ Refactoring/statement.js
@@ -29,6 +29,18 @@ function playFor(aPerformance) {
     return plays[aPerformance.playID];
 }
 
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    if ("comedy" === playFor(aPerformance).type) {
+        result += Math.floor(aPerformance.audience / 5)
+    }
+
+    return result;
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -36,14 +48,9 @@ function statement(invoice, plays) {
     const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
-
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
-
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience})석`;
-        totalAmount += thisAmount;
-
+        volumeCredits += volumeCreditsFor(perf);
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience})석`;
+        totalAmount += amountFor(perf);
     }
 
     result += `총액: ${format(totalAmount / 100)}\n`;
