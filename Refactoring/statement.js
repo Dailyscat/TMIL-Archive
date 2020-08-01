63a8fc2a08f8afe56c2c91c2a07354c36472bc03
dailyscat <pay_nack@naver.com>
2020-08-01 17:52:47 +0900

    totalVolumeCredit 함수 추출

--- Refactoring/statement.js
+++ Refactoring/statement.js
@@ -45,19 +45,26 @@ function usd(aNumber) {
     return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
+function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
+    return volumeCredits;
+}
+
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience})석`;
         totalAmount += amountFor(perf);
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점 \n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
     return result
 }
 
